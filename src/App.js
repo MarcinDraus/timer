baa95c1d26f4cacaebc3778a834386183282e144
MarcinDraus <marcin.drauss@gmail.com>
2023-06-17 00:37:46 +0200

    stopwatch clean code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-//import './global.scss';
 import AppDuble from './Components/AppDuble/AppDuble';
-//import FormattedTime from './Components/FormattedTime/FormattedTime';
 import Container from './Components/Container/Container';
-import './styles/normalize.scss';
-import './styles/global.scss';
-
 
 const App = () => {
   
   return (
   <Container>
-   
-    <AppDuble />
-  </Container>
-      
-      
-    
-    
+       <AppDuble />
+  </Container>    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import FormattedTime from './Components/FormattedTime/FormattedTime';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
