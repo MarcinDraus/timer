4a2a0b803a29bcd39f1b9fec4362912e9a3e6d92
MarcinDraus <marcin.drauss@gmail.com>
2023-06-16 22:21:49 +0200

    stopwatch

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import styles from './Button.module.scss';
 
-const Button = props => {
-    return <button className={styles.button}>{props.children}</button>;
+import styles from './Button.module.scss';
+import React from 'react';
+
+const Button = (props) => {
+  return <button className={styles.button} onClick={props.onClick}>{props.children}</button>;
 };
 
 export default Button;
